bb2270f81e040b42dead682a19bc39374e0b982d
William <wgatson@ufl.edu>
2024-03-01 05:28:25 -0500

    Questions

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Your code here
+var questionsArr = [
+    {
+      question: 'What is the capital city of Australia',
+      answer: 'Canberra',
+      options: [
+        'Sydney ',
+        'Melbourne',
+        'Canberra',
+        'Brisbane'
+
+      ]
+    },
+    {
+        question: 'Who wrote the famous novel "Pride and Prejudice"? ',
+        answer: 'Jane Austen',
+        options: [
+          'Charles Dickens',
+          'Jane Austen',
+            'Mark Twain',
+            'William Shakespeare'
+        ]
+      },
+      {
+        question: 'What is the chemical symbol for gold?',
+        answer: 'Au',
+        options: [
+            'Ag',
+            'Au',
+            'Cu',
+            'Pt'
+        ]
+      },
+      {
+        question: 'In which year did the Titanic sink?',
+        answer: '1912',
+        options: [
+            '1910',
+            '1912',
+            '1914',
+            '1916'
+        ]
+      },
+      {
+        question: 'Who painted the famous artwork "The Starry Night"?',
+        answer: 'Vincent van Gogh',
+        options: [
+            'Pablo Picasso',
+            'Vincent van Gogh',
+            'Leonardo da Vinci',
+            'Claude Monet'
+        ]
+      },
+  ]
+
+  
+
+ 
